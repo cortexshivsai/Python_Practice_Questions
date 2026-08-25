61575b22131949aee85b821df3b4aee77df1fbb8
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-25 23:09:58 +0530

    Delete 08_simple_interest.py

--- 08_simple_interest.py
+++
@@ -1,7 +0,0 @@
-#Calculate simple interest
-principal_amt=int(input("Enter the principal amount: "))
-rate_of_interest=float(input("Enter rate of interest(in percentage): "))
-time_period=int(input("Enter Time Period(in years): "))
-simple_interest=(principal_amt*rate_of_interest*time_period)/100
-print(f"The simple interest is: {simple_interest}")
-print(f"The total amount with interest is: {principal_amt+simple_interest}")
