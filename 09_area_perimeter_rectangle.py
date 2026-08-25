b6625fd5289a029fda9eb3f331ea80e0571cd011
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-25 23:10:08 +0530

    Delete 09_area_perimeter_rectangle.py

--- 09_area_perimeter_rectangle.py
+++
@@ -1,5 +0,0 @@
-#Calculate the area and perimeter of a rectangle.
-length=float(input("Enter length of the rectangle: "))
-breadth=float(input("Enter breadth of the recctangle: "))
-print(f"Area of the rectangle is: {length*breadth}")
-print(f"Perimeter of the triangle is: {2*length+2*breadth}")
