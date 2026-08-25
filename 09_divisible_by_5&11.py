ea7f33b3a85d8ce8292772703437bfb8d0764790
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-25 23:10:17 +0530

    Delete 09_divisible_by_5&11.py

--- 09_divisible_by_5&11.py
+++
@@ -1,6 +0,0 @@
-#Check divisible by both 5 and 11.
-num=int(input("Enter any number: "))
-if num%5==0 and num%11==0:
-    print(f"Number {num} is divisible by both 5 and 11")
-else:
-    print(f"Number {num} is not divisible by both 5 and 11")
