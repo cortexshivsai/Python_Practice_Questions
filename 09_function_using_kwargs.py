a135483b69e879233040f042a68ebb11f871d053
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-25 23:11:51 +0530

    Delete 09_function_using_kwargs.py

--- 09_function_using_kwargs.py
+++
@@ -1,9 +0,0 @@
-#Function using **kwargs 
-def student(**kwargs):
-
-    for key, value in kwargs.items():
-        print(key, ":", value)
-
-student(name="Rahul", age=20, marks=85)
-
-#**kwargs → multiple keyword arguments   → dictionary
