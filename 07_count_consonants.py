fcede6a4c25068c2e05547a503d4e064e3147d3d
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-26 17:48:06 +0530

    Delete 07_count_consonants.py

--- 07_count_consonants.py
+++
@@ -1,9 +0,0 @@
-#Count consonants.
-string = input("Enter a string: ")
-count = 0
-string = string.lower()
-for char in string:
-    if  char not in "aeiouAEIOU":
-        count += 1
-
-print("Number of consonants:", count)
