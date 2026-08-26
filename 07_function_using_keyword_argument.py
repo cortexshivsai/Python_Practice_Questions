7222af5cd0d41ba6324415d2eb070823fe0f230e
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-26 17:46:42 +0530

    ..

--- 07_function_using_keyword_argument.py
+++
@@ -1,8 +0,0 @@
-#Function using keyword argument
-def student(name, age):
-    print("Name:", name)
-    print("Age:", age)
-
-age=int(input("Enter age: "))
-name=input("Enter Name: ")
-student(name , age)
