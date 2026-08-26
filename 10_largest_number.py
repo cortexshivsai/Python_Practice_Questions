9a2aacc1d08f2e00f8c37a353ecc8f981ed094d7
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-26 17:44:25 +0530

    ..

--- 10_largest_number.py
+++
@@ -1,6 +0,0 @@
-#Take three numbers and print the largest.
-num1=int(input("Enter first number: "))
-num2=int(input("Enter second number: "))
-num3=int(input("Enter third number: "))
-max_number=max(num1,num2,num3)
-print(f"The largest number is: {max_number}") #or print(f"The largest number is: {max(num1,num2,num3)}")
