a1ed8be3a31c6648db4d7d95e748cc043530a0b2
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-26 17:55:43 +0530

    Delete 04_largest_of_two.py

--- 04_largest_of_two.py
+++
@@ -1,9 +0,0 @@
-#Find largest of two numbers.
-num1=int(input("Enter first number: "))
-num2=int(input("Enter second number: "))
-if num1>num2:
-    print(f"The First Number {num1} is Largest")
-elif num1==num2:
-    print(f"Both Numbers {num1} and {num2} are Equal")  
-else:
-    print(f"The Second Number {num2} is Largest")      
