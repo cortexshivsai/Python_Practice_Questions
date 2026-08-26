c281f6930187e8e470ad1efbcf30940c19d01def
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-26 17:52:40 +0530

    Delete 05_list_to_tuple.py

--- 05_list_to_tuple.py
+++
@@ -1,7 +0,0 @@
-#Convert list into tuple.
-a=[1,2,3,4,5,6,7,8,9,10]
-print(f"List is: {a}")
-print(f"Before Conversion: {type(a)}")
-b=tuple(a)
-print(f"Tuple is: {b}")
-print(f"After Conversion: {type(b)}")
