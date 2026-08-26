eedc78d57b43f7f3ba56d00d439d24b20eec41ec
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-26 17:50:56 +0530

    Delete 06_count_vowels.py

--- 06_count_vowels.py
+++
@@ -1,8 +0,0 @@
-string = input("Enter a string: ")
-count=0
-for i in string:
-    if 'a' or 'e' or 'i' or 'o' 'u' or 'A' or 'E' or 'I' or 'O' or 'U' in string:
-        count=count+1
-    else:
-        pass    
-print("No.of vowels are: ",count)        
