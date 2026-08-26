601d707ba5e39d3ecbc91a715e161180152179e4
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-26 17:54:50 +0530

    Delete 04_sum.py

--- 04_sum.py
+++
@@ -1,4 +0,0 @@
-#Take two numbers as input and print their sum.
-num1=int(input("Enter First Number: "))
-num2=int(input("Enter Second Number: "))
-print(f"The sum of the two numbers is {num1+num2}")
