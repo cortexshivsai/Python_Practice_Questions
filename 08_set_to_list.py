a69a3cf4af41f50e4ec6d9db9b5d79777e3e94d5
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-26 17:39:37 +0530

    ..

--- 08_set_to_list.py
+++
@@ -1,7 +0,0 @@
-#Convert set into list.
-a={1,2,3,4,5,6,7,8,9,10}
-print(f"Set is: {a}")
-print(f"Before Conversion: {type(a)}")
-b=list(a)
-print(f"After Conversion: {type(b)}")
-print(f"List is: {b}")
