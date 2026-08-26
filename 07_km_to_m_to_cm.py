ac9493e0e7103e4fc53c38a56fa500cd6c5df288
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-26 17:46:12 +0530

    ..

--- 07_km_to_m_to_cm.py
+++
@@ -1,5 +0,0 @@
-#Convert kilometers into meters and centimeters.
-kilometers=int(input("Enter Distance in Kilometers: "))
-meters=(kilometers*1000)
-centimeters=(kilometers*100000)
-print(f"Distance in meters: {meters}m and Distance in Centimeters is: {centimeters}cm")
