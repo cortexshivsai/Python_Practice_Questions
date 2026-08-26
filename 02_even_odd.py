511b69a4433c299021baee6cbaadfcbfb8286a17
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-26 18:01:00 +0530

    Delete 02_even_odd.py

--- 02_even_odd.py
+++
@@ -1,8 +0,0 @@
-#Check even or odd
-num=int(input("Enter any number: "))
-if num%2==0:
-    print(f"The number {num} is Even")
-elif num==0:
-    print(f"The number {num} is Zero") 
-else:
-    print(f"The number {num} is odd")       
