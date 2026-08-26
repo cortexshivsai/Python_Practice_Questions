1ca643acfc57d721edd2c888a3ccaff7d1e850b3
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-26 17:59:35 +0530

    Delete 02_even_numbers_upto_100.py

--- 02_even_numbers_upto_100.py
+++
@@ -1,5 +0,0 @@
-#Print even numbers till 100.
-print("Even numbers from 1 to 100 are: ")
-for i in range(1,101):
-    if i%2==0:
-        print(i,end=" ")# Prints the value of i followed by a space instead of a newline
