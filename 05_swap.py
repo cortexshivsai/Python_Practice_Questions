c77d1eeffa395440aad61027bd84052ad8839045
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-26 17:51:27 +0530

    Delete 05_swap.py

--- 05_swap.py
+++
@@ -1,8 +0,0 @@
-#Swap two variables without using a third variable.
-a="Shiv"
-b="Sai"
-print("Before Swapping:")
-print(f"a = {a} and b = {b}")
-a,b=b,a
-print("After Swapping:")
-print(f"a = {a} and b = {b}")
